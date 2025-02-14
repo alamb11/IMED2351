c643a6277dc12b585e3f76b2f5d03037106c90f9
alamb11 <alana.lamb55@gmail.com>
2025-02-13 19:08:06 -0600

    Create modulefour.js

---
+++ modulefour.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+const name = "Alana";
+const age = 20;
+const birthday = "February 29";
+const cat = "Robbie";
+
+console.log(`Hello my name is ${name} and I am ${age} years old. My birthday is on ${birthday} and I have a cat named ${cat}`)
